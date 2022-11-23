88a592318f1b5c2b2345f7bd104cd31205d9b057
AlexDerr <alex.derr999@gmail.com>
2022-11-22 22:58:45 -0500

    Updating through K'Sante
    - Updated readme
    - Updated some incorrect lists
    - Added in the Variety's Overrated challenge
    - Updated result file names as there are now comps up to 8

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # ChallengeComps
 
-Tool to create a list of the League of Legends team compositions that would count for the largest amount of 5-stack team composition challenges. 
+A tool to create a list of the League of Legends team compositions that would count for the largest amount of 5-stack team composition challenges. 
 
-Due to the mostly static nature of the input/output to the program and large run time, output files have been included in the repository. `5-6.zip` contains a `.txt` file containing a reduced list that has a list of compositions that count for 5 or 6 challenges and `2-6.zip` contains a `.txt` file with a list of compositions counting for 2 to 6.
+Due to the mostly static nature of the input/output to the program and large run time, output files have been included in the repository. `5+.zip` contains a `.txt` file containing a reduced list that has a list of compositions that count for 5 or more challenges and `3+.zip` contains a `.txt` file with a list of compositions counting for 3 or more.
 
 The output of `ChallengeComps.py` is a text file `output.txt` containing team compositions along with the challenges that they count for. These are provided 1 per line and they are sorted by the number of challenges they count for.
 ```
@@ -34,9 +34,9 @@ The output of `ChallengeComps.py` is a text file `output.txt` containing team co
 - targon - Peak Performance
 - void - (Inhuman Screeching Sounds)
 - zaun - Chemtech Comrades
+- oneClass - Variety's Overrated
 
 5-stack challenges that are not considered by this tool
-- Variety's Overrated
-- Any others not on this list
+- Any not on this list
 
-As new champions are released they would simply need to be added `allChamplist` and then added into the list of each challenge that they count for.
+As new champions are released they will need to be added `allChamplist` and then added into the list of each challenge that they count for.
--- cli/ChallengeComps.py
+++ cli/ChallengeComps.py
@@ -7,65 +7,67 @@ allChampList = [
 	'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank',
 	'Garen', 'Gnar', 'Gragas', 'Graves', 'Gwen', 'Hecarim', 'Heimerdinger',
 	'Illaoi', 'Irelia', 'Ivern', 'Janna', 'Jarvan IV', 'Jax', 'Jayce',
-	'Jhin', 'Jinx', 'Kai\'Sa', 'Kalista', 'Karma', 'Karthus', 'Kassadin',
-	'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Kha\'Zix', 'Kindred', 'Kled', 
-	'Kog\'Maw', 'LeBlanc', 'Lee Sin', 'Leona', 'Lillia', 'Lissandra', 'Lucian', 
-	'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master Yi', 'Miss Fortune', 
-	'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nidalee', 
-	'Nocturne', 'Nunu & Willump', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 
-	'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Rek\'Sai', 'Rell', 
-	'Renata Glasc', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Samira', 
-	'Sejuani', 'Senna', 'Seraphine', 'Sett', 'Shaco', 'Shen', 'Shyvana', 
-	'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 
-	'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah', 'Talon', 'Taric', 'Teemo', 
-	'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Twitch', 'Udyr', 
-	'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vel\'Koz', 'Vex', 'Vi', 
-	'Viego', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 'Wukong', 
-	'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yone', 'Yorick', 'Yuumi', 
-	'Zac', 'Zed', 'Zeri', 'Ziggs', 'Zilean', 'Zoe', 'Zyra'
+	'Jhin', 'Jinx', 'K\'Sante', 'Kai\'Sa', 'Kalista', 'Karma', 'Karthus', 
+	'Kassadin', 'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Kha\'Zix', 'Kindred', 
+	'Kled', 'Kog\'Maw', 'LeBlanc', 'Lee Sin', 'Leona', 'Lillia', 'Lissandra',
+	'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master Yi',
+	'Miss Fortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko',
+	'Nidalee', 'Nilah', 'Nocturne', 'Nunu & Willump', 'Olaf', 'Orianna', 'Ornn',
+	'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus',
+	'Rek\'Sai', 'Rell', 'Renata Glasc', 'Renekton', 'Rengar', 'Riven', 'Rumble',
+	'Ryze', 'Samira', 'Sejuani', 'Senna', 'Seraphine', 'Sett', 'Shaco',
+	'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona',
+	'Soraka', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah', 'Talon',
+	'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate',
+	'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vel\'Koz',
+	'Vex', 'Vi', 'Viego', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 
+	'Wukong', 'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yone', 'Yorick',
+	'Yuumi', 'Zac', 'Zed', 'Zeri', 'Ziggs', 'Zilean', 'Zoe',
+	'Zyra'
 ]
 
 # List of champions that count for each challenge
 challengeLists = {
-	'aoe' : [
-		'Amumu', 'Annie', 'Aurelion Sol', 'Azir', 'Bard', 'Bel\'Veth', 'Blitzcrank',
-		'Braum', 'Cassiopeia', 'Corki', 'Diana', 'Draven', 'Ekko', 'Ezreal',
-		'Fiddlesticks', 'Fizz', 'Galio', 'Gangplank', 'Gnar', 'Gragas', 'Graves',
-		'Hecarim', 'Heimerdinger', 'Illaoi', 'Janna', 'Jarvan IV', 'Jinx', 'Karthus',
-		'Katarina', 'Kayle', 'Kennen', 'Leona', 'Lissandra', 'Lux', 'Malphite',
-		'Maokai', 'Miss Fortune', 'Morgana', 'Nami', 'Neeko', 'Nunu & Willump', 'Orianna',
-		'Ornn', 'Pantheon', 'Qiyana', 'Rakan', 'Rammus', 'Rell', 'Renata Glasc',
-		'Riven', 'Rumble', 'Samira', 'Sejuani', 'Senna', 'Seraphine', 'Sett',
-		'Sona', 'Swain', 'Talon', 'Twitch', 'Vel\'Koz', 'Viktor', 'Vladimir',
-		'Volibear', 'Xayah', 'Xin Zhao', 'Yone', 'Yuumi', 'Zac', 'Ziggs',
-		'Zyra'
+	'aoe': [
+		'Amumu', 'Anivia', 'Annie', 'Aurelion Sol', 'Azir', 'Bard', 'Bel\'Veth', 
+		'Blitzcrank', 'Braum', 'Cassiopeia', 'Corki', 'Diana', 'Draven', 'Ekko',
+		'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fizz', 'Galio', 'Gangplank', 'Gnar',
+		'Gragas', 'Graves', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Janna', 'Jarvan IV',
+		'Jinx', 'Karthus', 'Katarina', 'Kayle', 'Kennen', 'Leona', 'Lissandra',
+		'Lux', 'Malphite', 'Maokai', 'Miss Fortune', 'Morgana', 'Nami', 'Neeko',
+		'Nilah', 'Nunu & Willump', 'Orianna', 'Ornn', 'Pantheon', 'Qiyana', 'Rakan',
+		'Rammus', 'Rell', 'Renata Glasc', 'Riven', 'Rumble', 'Samira', 'Sejuani',
+		'Senna', 'Seraphine', 'Sett', 'Sona', 'Swain', 'Talon', 'Twitch',
+		'Vel\'Koz', 'Viktor', 'Vladimir', 'Volibear', 'Xayah', 'Xin Zhao', 'Yone',
+		'Yuumi', 'Zac', 'Ziggs', 'Zyra'
 	],
 
-	'heal' : [
+	'heal': [
 		'Alistar', 'Annie', 'Bard', 'Fiora', 'Galio', 'Ivern', 'Janna',
-		'Karma', 'Kayle', 'Kindred', 'Lee Sin', 'Lulu', 'Lux', 'Morgana',
-		'Nami', 'Nidalee', 'Orianna', 'Rakan', 'Rell', 'Renata Glasc', 'Senna',
-		'Seraphine', 'Shen', 'Sona', 'Soraka', 'Tahm Kench', 'Taric', 'Thresh',
-		'Yuumi', 'Zilean'
+		'K\'Sante', 'Karma', 'Kayle', 'Kindred', 'Lee Sin', 'Lulu', 'Lux',
+		'Morgana', 'Nami', 'Nidalee', 'Nilah', 'Orianna', 'Rakan', 'Rell',
+		'Renata Glasc', 'Senna', 'Seraphine', 'Shen', 'Sona', 'Soraka', 'Tahm Kench',
+		'Taric', 'Thresh', 'Yuumi', 'Zilean'
 	],
 
-	'poke' : [
+	'poke': [
 		'Akshan', 'Ashe', 'Azir', 'Caitlyn', 'Cho\'Gath', 'Corki', 'Dr. Mundo',
 		'Ezreal', 'Gangplank', 'Jayce', 'Jhin', 'Jinx', 'Kai\'Sa', 'Karma',
 		'Kled', 'Kog\'Maw', 'Lux', 'Maokai', 'Nidalee', 'Pantheon', 'Senna',
 		'Seraphine', 'Shyvana', 'Sivir', 'Taliyah', 'Twisted Fate', 'Varus', 'Vel\'Koz',
-		'Vex', 'Viktor', 'Xayah', 'Xerath', 'Yuumi', 'Ziggs', 'Zoe'
+		'Vex', 'Viktor', 'Xayah', 'Xerath', 'Yuumi', 'Zeri', 'Ziggs',
+		'Zoe'
 	],
 
-	'displacement' : [
+	'displacement': [
 		'Alistar', 'Aurelion Sol', 'Azir', 'Blitzcrank', 'Darius', 'Gnar', 'Gragas',
-		'Hecarim', 'Janna', 'Jayce', 'Kled', 'Lee Sin', 'Maokai', 'Mordekaiser',
-		'Nautilus', 'Orianna', 'Poppy', 'Pyke', 'Rell', 'Sett', 'Singed',
-		'Skarner', 'Swain', 'Tahm Kench', 'Taliyah', 'Thresh', 'Tristana', 'Urgot',
-		'Vayne', 'Xin Zhao', 'Yone', 'Zac', 'Ziggs'
+		'Hecarim', 'Janna', 'Jayce', 'K\'Sante', 'Kled', 'Lee Sin', 'Maokai', 
+		'Mordekaiser', 'Nautilus', 'Nilah', 'Orianna', 'Poppy', 'Pyke', 'Rell',
+		'Sett', 'Singed', 'Skarner', 'Swain', 'Tahm Kench', 'Taliyah', 'Thresh',
+		'Tristana', 'Urgot', 'Vayne', 'Xin Zhao', 'Yone', 'Zac', 'Ziggs'
 	],
 
-	'globals' : [
+	'globals': [
 		'Akshan', 'Ashe', 'Bard', 'Caitlyn', 'Draven', 'Ezreal', 'Galio',
 		'Gangplank', 'Jhin', 'Jinx', 'Kai\'Sa', 'Karthus', 'Kled', 'Lillia',
 		'Lux', 'Maokai', 'Nocturne', 'Pantheon', 'Ryze', 'Senna', 'Shen',
@@ -73,108 +75,174 @@ challengeLists = {
 		'Ziggs'
 	],
 
-	'revive' : [
+	'revive': [
 		'Akshan', 'Alistar', 'Anivia', 'Bard', 'Bel\'Veth', 'Braum', 'Ekko', 
-		'Elise', 'Evelynn', 'Fiora', 'Fizz', 'Gwen', 'Jax', 'Kalista', 
-		'Karthus', 'Kayle', 'Kayn', 'Kindred', 'Kled', 'Kog\'Maw', 'Lissandra', 
-		'Malzahar', 'Maokai', 'Master Yi', 'Olaf', 'Pantheon', 'Rakan', 'Renata Glasc', 
-		'Sion', 'Tahm Kench', 'Taric', 'Tryndamere', 'Vladimir', 'Xin Zhao', 'Yuumi', 
-		'Zac', 'Zed', 'Zilean'
+		'Elise', 'Evelynn', 'Fiora', 'Fizz', 'Gwen', 'Jax', 'K\'Sante', 
+		'Kalista', 'Karthus', 'Kayle', 'Kayn', 'Kindred', 'Kled', 'Kog\'Maw',
+		'Lissandra', 'Malzahar', 'Maokai', 'Master Yi', 'Olaf', 'Pantheon', 'Rakan',
+		'Renata Glasc', 'Sion', 'Tahm Kench', 'Taric', 'Tryndamere', 'Vladimir', 'Xin Zhao',
+		'Yuumi', 'Zac', 'Zed', 'Zilean'
 	],
 
-	'immobilizing' : [
+	'immobilizing': [
 		'Aatrox', 'Alistar', 'Amumu', 'Anivia', 'Aurelion Sol', 'Bard', 'Blitzcrank', 
 		'Braum', 'Camille', 'Galio', 'Gnar', 'Gragas', 'Hecarim', 'Ivern',
-		'Janna', 'Jarvan IV'
+		'Janna', 'Jarvan IV', 'K\'Sante', 'Kled', 'LeBlanc', 'Leona', 'Lissandra',
+		'Lulu', 'Maokai', 'Morgana', 'Nami', 'Nautilus', 'Neeko', 'Nunu & Willump',
+		'Ornn', 'Poppy', 'Pyke', 'Qiyana', 'Rammus', 'Rell', 'Renata Glasc', 
+		'Riven', 'Sejuani', 'Seraphine', 'Sett', 'Shaco', 'Singed', 'Sion',
+		'Skarner', 'Tahm Kench', 'Thresh', 'Urgot', 'Vi', 'Warwick', 'Xin Zhao',
+		'Yasuo', 'Yone', 'Zac', 'Zyra'
 	],
 
-	'stealth' : [
+	'stealth': [
 		'Akali', 'Akshan', 'Evelynn', 'Kai\'Sa', 'Kha\'Zix', 'LeBlanc', 'Neeko', 
 		'Nocturne', 'Pyke', 'Qiyana', 'Rengar', 'Senna', 'Shaco', 'Talon', 
 		'Teemo', 'Twitch', 'Vayne', 'Viego'
 	],
 
-	'summon' : [
+	'summon': [
 		'Annie', 'Azir', 'Elise', 'Fiddlesticks', 'Heimerdinger', 'Illaoi', 'Ivern',
 		'Kalista', 'Kindred', 'Lissandra', 'Malzahar', 'Maokai', 'Neeko', 'Orianna',
 		'Shaco', 'Yorick', 'Zed', 'Zyra'
 	],
 
-	'trap' : [
+	'trap': [
 		'Caitlyn', 'Gangplank', 'Jhin', 'Jinx', 'Maokai', 'Nidalee', 'Shaco', 
 		'Teemo', 'Ziggs', 'Zyra'
 	],
 
-	'terrain' : [
+	'terrain': [
 		'Anivia', 'Azir', 'Irelia', 'Ivern', 'Jarvan IV', 'Ornn', 'Taliyah', 
 		'Trundle', 'Veigar', 'Yorick'
 	],
 
-	'shurima' : [
-		'Akshan', 'Amumu', 'Azir', 'Nasus', 'Rammus', 'Renekton', 'Sivir',
-		'Skarner', 'Taliyah', 'Xerath', 'Zilean'
+	# K'Sante not currently listed
+	'shurima': [
+		'Akshan', 'Amumu', 'Azir', 'K\'Sante', 'Nasus', 'Rammus', 'Renekton',
+		'Sivir', 'Skarner', 'Taliyah', 'Xerath', 'Zilean'
 	],
 
-	'ionia' : [
+	'ionia': [
 		'Ahri', 'Akali', 'Irelia', 'Ivern', 'Jhin', 'Karma', 'Kayn',
 		'Kennen', 'Lee Sin', 'Lillia', 'Master Yi', 'Rakan', 'Sett', 'Shen',
 		'Syndra', 'Varus', 'Wukong', 'Xayah', 'Yasuo', 'Yone', 'Zed'
 	],
 
-	'bandle' : [
+	'bandle': [
 		'Corki', 'Fizz', 'Gnar', 'Heimerdinger', 'Kennen', 'Kled', 'Lulu',
 		'Poppy', 'Rumble', 'Teemo', 'Tristana', 'Veigar', 'Vex', 'Yuumi',
 		'Ziggs'
 	],
 
-	'bilgewater' : [
-		'Fizz', 'Gangplank', 'Graves', 'Illaoi', 'Miss Fortune', 'Nautilus', 'Pyke', 
-		'Tahm Kench', 'Twisted Fate'
+	'bilgewater': [
+		'Fizz', 'Gangplank', 'Graves', 'Illaoi', 'Miss Fortune', 'Nautilus', 'Nilah',
+		'Pyke', 'Tahm Kench', 'Twisted Fate'
 	],
 
-	'demacia' : [
+	'demacia': [
 		'Fiora', 'Galio', 'Garen', 'Jarvan IV', 'Kayle', 'Lucian', 'Lux',
 		'Morgana', 'Poppy', 'Quinn', 'Shyvana', 'Sona', 'Sylas', 'Vayne',
 		'Xin Zhao'
 	],
 
-	'freljord' : [
+	'freljord': [
 		'Anivia', 'Ashe', 'Braum', 'Gnar', 'Gragas', 'Lissandra', 'Nunu & Willump',
 		'Olaf', 'Ornn', 'Sejuani', 'Trundle', 'Tryndamere', 'Udyr', 'Volibear'
 	],
 
-	'ixtal' : [
+	'ixtal': [
 		'Malphite', 'Neeko', 'Nidalee', 'Qiyana', 'Rengar', 'Zyra'
 	],
 
-	'noxus' : [
+	'noxus': [
 		'Cassiopeia', 'Darius', 'Draven', 'Katarina', 'Kled', 'LeBlanc', 'Rell',
 		'Riven', 'Samira', 'Sion', 'Swain', 'Talon', 'Vladimir'
 	],
 
-	'piltover' : [
+	'piltover': [
 		'Caitlyn', 'Camille', 'Corki', 'Ezreal', 'Heimerdinger', 'Jayce', 'Orianna',
 		'Seraphine', 'Vi'
 	],
 
-	'shadowIsles' : [
+	'shadowIsles': [
 		'Elise', 'Evelynn', 'Fiddlesticks', 'Gwen', 'Hecarim', 'Kalista', 'Karthus', 
 		'Maokai', 'Senna', 'Thresh', 'Viego', 'Yorick'
 	],
 
-	'targon' : [
+	'targon': [
 		'Aphelios', 'Aurelion Sol', 'Diana', 'Leona', 'Pantheon', 'Soraka', 'Taric', 
 		'Zoe'
 	],
 
-	'void' : [
+	'void': [
 		'Bel\'Veth', 'Cho\'Gath', 'Kai\'Sa', 'Kassadin', 'Kha\'Zix', 'Kog\'Maw', 'Malzahar', 
 		'Rek\'Sai', 'Vel\'Koz'
 	],
 
-	'zaun' : [
+	'zaun': [
 		'Blitzcrank', 'Dr. Mundo', 'Ekko', 'Janna', 'Jinx', 'Renata Glasc', 'Singed', 
 		'Twitch', 'Urgot', 'Viktor', 'Warwick', 'Zac', 'Zeri', 'Ziggs'
+	],
+
+	'assassin': [
+		'Ahri', 'Akali', 'Akshan', 'Ekko', 'Evelynn', 'Fiora', 'Fizz',
+		'Gwen', 'Irelia', 'Jax', 'Kassadin', 'Katarina', 'Kayn', 'Kha\'Zix',
+		'LeBlanc', 'Lee Sin', 'Malzahar', 'Master Yi', 'Nidalee', 'Nilah', 'Nocturne',
+		'Pantheon', 'Pyke', 'Qiyana', 'Quinn', 'Rengar', 'Riven', 'Shaco',
+		'Sylas', 'Talon', 'Teemo', 'Tristana', 'Tryndamere', 'Twitch', 'Vayne',
+		'Vi', 'Viego', 'Xin Zhao', 'Yasuo', 'Yone', 'Zed'
+	],
+
+	'mage': [
+		'Ahri', 'Amumu', 'Anivia', 'Annie', 'Aurelion Sol', 'Azir', 'Bard',
+		'Brand', 'Cassiopeia', 'Cho\'Gath', 'Diana', 'Elise', 'Evelynn', 'Ezreal',
+		'Fiddlesticks', 'Galio', 'Gragas', 'Heimerdinger', 'Ivern', 'Janna', 'Jhin',
+		'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kennen', 'Kog\'Maw', 'LeBlanc',
+		'Lillia', 'Lissandra', 'Lulu', 'Lux', 'Malzahar', 'Maokai', 'Morgana',
+		'Nami', 'Neeko', 'Nidalee', 'Orianna', 'Renata Glasc', 'Rumble', 'Ryze',
+		'Seraphine', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'Taliyah',
+		'Twisted Fate', 'Varus', 'Veigar', 'Vel\'Koz', 'Vex', 'Viktor', 'Vladimir',
+		'Xerath', 'Yuumi', 'Ziggs', 'Zilean', 'Zoe', 'Zyra'
+	],
+
+	'marksman': [
+		'Akshan', 'Aphelios', 'Ashe', 'Azir', 'Caitlyn', 'Corki', 'Draven',
+		'Ezreal', 'Graves', 'Jayce', 'Jhin', 'Jinx', 'Kai\'Sa', 'Kalista',
+		'Kennen', 'Kindred', 'Kog\'Maw', 'Lucian', 'Miss Fortune', 'Quinn', 'Samira',
+		'Senna', 'Sivir', 'Teemo', 'Tristana', 'Twitch', 'Varus', 'Vayne',
+		'Xayah', 'Zeri'
+	],
+
+	'tank': [
+		'Aatrox', 'Alistar', 'Amumu', 'Blitzcrank', 'Braum', 'Camille', 'Cho\'Gath',
+		'Darius', 'Dr. Mundo', 'Galio', 'Garen', 'Gnar', 'Hecarim', 'Illaoi',
+		'Jarvan IV', 'K\'Sante', 'Kled', 'Leona', 'Malphite', 'Maokai', 'Nasus',
+		'Nautilus', 'Nunu & Willump', 'Olaf', 'Ornn', 'Poppy', 'Rammus', 'Rell',
+		'Renekton', 'Sejuani', 'Sett', 'Shen', 'Shyvana', 'Singed', 'Sion',
+		'Skarner', 'Tahm Kench', 'Trundle', 'Udyr', 'Urgot', 'Volibear', 'Warwick',
+		'Wukong', 'Yorick', 'Zac'
+	],
+
+	'support': [
+		'Alistar', 'Anivia', 'Ashe', 'Bard', 'Braum', 'Fiddlesticks', 'Heimerdinger',
+		'Ivern', 'Janna', 'Karma', 'Kayle', 'Leona', 'Lulu', 'Lux',
+		'Morgana', 'Nami', 'Nautilus', 'Neeko', 'Orianna', 'Pyke', 'Rakan',
+		'Rell', 'Renata Glasc', 'Senna', 'Seraphine', 'Sona', 'Soraka', 'Tahm Kench',
+		'Taliyah', 'Taric', 'Thresh', 'Yuumi', 'Zilean', 'Zoe', 'Zyra'
+	],
+
+	'fighter': [
+		'Aatrox', 'Bel\'Veth', 'Blitzcrank', 'Camille', 'Darius', 'Diana', 'Dr. Mundo',
+		'Ekko', 'Elise', 'Fiora', 'Fizz', 'Gangplank', 'Garen', 'Gnar',
+		'Gragas', 'Gwen', 'Hecarim', 'Illaoi', 'Irelia', 'Jarvan IV', 'Jax',
+		'Jayce', 'K\'Sante', 'Kayle', 'Kayn', 'Kled', 'Lee Sin', 'Lillia',
+		'Malphite', 'Master Yi', 'Mordekaiser', 'Nasus', 'Nilah', 'Nocturne', 'Nunu & Willump',
+		'Olaf', 'Ornn', 'Pantheon', 'Poppy', 'Qiyana', 'Rammus', 'Rek\'Sai',
+		'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sejuani', 'Sett',
+		'Shyvana', 'Singed', 'Sion', 'Skarner', 'Swain', 'Taric', 'Thresh',
+		'Trundle', 'Tryndamere', 'Udyr', 'Urgot', 'Vi', 'Viego', 'Volibear',
+		'Warwick', 'Wukong', 'Xin Zhao', 'Yasuo', 'Yone', 'Yorick', 'Zac'
 	]
 }
 
@@ -252,9 +320,12 @@ for comp in itertools.combinations(allChampList, 5):
 		challenges.append('void')
 	if(counts['zaun'] >= 5):
 		challenges.append('zaun')
+	if(counts['assassin'] >= 5 or counts['mage'] >= 5 or counts['marksman'] >= 5 or counts['tank'] >= 5
+		or counts['support'] >= 5 or counts['figher'] >= 5):
+		challenges.append('oneClass')
 
-	# Only saving the compositions that counts for 2 or more challenges to save on memory and storage requirements
-	if(len(challenges) > 2):
+	# Only saving the compositions that counts for x or more challenges to save on memory and storage requirements if needed
+	if(len(challenges) > 3):
 		compsDict[str(comp)] = str(challenges)
 
 	compsChecked += 1
