eb89eed011c55dc9b9e0d875d061384fad05e9d3
AlexDerr <alex.derr999@gmail.com>
2024-09-08 23:07:19 -0400

    Script to get champion per challenge data from client api

---
+++ cli/data_update.py
@@ -0,0 +1,105 @@
+import base64
+import json
+import re
+import requests
+import subprocess
+import sys
+import urllib3
+
+urllib3.disable_warnings()
+
+HARMONY_CHALLENGE_ID = "303400"
+GLOBETROTTER_CHALLENGE_ID = "303500"
+ENTRY_PER_LINE = 7
+
+
+def pretty_str(data):
+    out_str = "{\n"
+    for key in data:
+        data[key].sort()
+        out_str += f"\t\"{key}\": ["
+        for entry in data[key]:
+            if (data[key].index(entry) % ENTRY_PER_LINE == 0
+                and data[key].index(entry) != len(data[key]) - 1
+            ):
+                out_str += "\n\t\t"
+
+            out_str += f"\"{entry}\""
+
+            if data[key].index(entry) != len(data[key]) - 1:
+                out_str += ", "
+
+            if data[key].index(entry) == len(data[key]) - 1:
+                out_str += "\n"
+
+                
+        out_str += "\t],\n"
+    out_str += "}"
+    out_str = out_str[:-3]
+    out_str += "\n}"
+
+    return out_str
+
+
+if __name__ == "__main__":
+    cmd_out = str(subprocess.check_output(["wmic", "PROCESS", "WHERE", "name='LeagueClientUx.exe'", "GET", "commandline"]))
+    app_port = re.search("--app-port=([0-9]*)", cmd_out)[0][11:]
+    auth_token = re.search("--remoting-auth-token=([\\w-]*)", cmd_out)[0][22:]
+    encoded_token = base64.b64encode(f'riot:{auth_token}'.encode()).decode()
+
+    champion_response = requests.get(
+        f"https://127.0.0.1:{app_port}/lol-champions/v1/owned-champions-minimal",
+        headers={
+            "Accept": "application/json",
+            "Authorization": f"Basic {encoded_token}",
+            "Content-Type": "application/json"
+        },
+        verify=False
+    )
+    
+    champion_id_dict = {}
+    champion_data_response = json.loads(champion_response.text)
+    for champion in champion_data_response:
+          champion_id_dict[champion["id"]] = champion["name"]
+
+    # with open("data/raw_champion_data.json", "w") as f:
+    #         f.write(json.dumps(champion_data_response, indent=4))
+    # with open("data/champion_dict.json", "w") as f:
+    #         f.write(json.dumps(champion_id_dict, indent=4))
+
+    challenge_response = requests.get(
+        f"https://127.0.0.1:{app_port}/lol-challenges/v1/challenges/local-player",
+        headers={
+            "Accept": "application/json",
+            "Authorization": f"Basic {encoded_token}",
+            "Content-Type": "application/json"
+        },
+        verify=False
+    )
+
+    raw_challenge_dict = json.loads(challenge_response.text)
+    # with open("data/challenge_data.json", "w") as f:
+    #         f.write(json.dumps(challenge_data_dict, indent=4))
+
+    if (HARMONY_CHALLENGE_ID not in raw_challenge_dict 
+        or GLOBETROTTER_CHALLENGE_ID not in raw_challenge_dict
+    ):
+        print("Harmony or Globetrotter ID not found. Exiting.")
+        sys.exit()
+
+    processed_challenge_dict = {}
+
+    all_champ_list = [champion_id_dict[key] for key in champion_id_dict]
+    all_champ_list.sort()
+    processed_challenge_dict["allChampList"] = all_champ_list
+
+    for challenge_id in raw_challenge_dict[HARMONY_CHALLENGE_ID]["childrenIds"] + raw_challenge_dict[GLOBETROTTER_CHALLENGE_ID]["childrenIds"]:
+        champion_ids = raw_challenge_dict[str(challenge_id)]["availableIds"]
+        champion_names = [champion_id_dict[champion_id] for champion_id in champion_ids if champion_id < 3000]
+        processed_challenge_dict[raw_challenge_dict[str(challenge_id)]["name"]] = champion_names
+
+    processed_challenge_dict["It Has Ultimate In the Name!"] = processed_challenge_dict.pop('It Has "Ultimate" In the Name!')
+    pretty_processed_str = pretty_str(processed_challenge_dict)
+
+    with open("data/input_data.json", "w") as f:
+        f.write(pretty_processed_str)
