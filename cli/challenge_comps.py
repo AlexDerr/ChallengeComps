8b99e125d0260c2f3a864da2d624290f28011aae
AlexDerr <alex.derr999@gmail.com>
2024-09-09 22:10:45 -0400

    Add config to filter out completed challenges

--- cli/challenge_comps.py
+++ cli/challenge_comps.py
@@ -13,6 +13,9 @@ if __name__ == "__main__":
 	with open("data/challenge_lists.json", "r") as f:
 		challenge_lists = json.load(f)
 
+	with open("config.json", "r") as f:
+		config = json.load(f)
+
 	list_gen_start = time.time()
 	all_champ_dict = {}
 
@@ -34,60 +37,67 @@ if __name__ == "__main__":
 		counts = Counter(all_champ_dict[comp[0]] + all_champ_dict[comp[1]] + all_champ_dict[comp[2]] + all_champ_dict[comp[3]] + all_champ_dict[comp[4]])
 		challenges = []
 
-		if(counts["It Has Ultimate In the Name!"] >= 3):
+		if(config["It Has Ultimate In the Name!"] and counts["It Has Ultimate In the Name!"] >= 3):
 			challenges.append("It Has Ultimate In the Name!")
-		if(counts["We Protec"] >= 3):
+		if(config["We Protec"] and counts["We Protec"] >= 3):
 			challenges.append("We Protec")
-		if(counts["We're Good Over Here"] >= 3):
+		if(config["We're Good Over Here"] and counts["We're Good Over Here"] >= 3):
 			challenges.append("We're Good Over Here")
-		if(counts["Get Over Here"] >= 3):
+		if(config["Get Over Here"] and counts["Get Over Here"] >= 3):
 			challenges.append("Get Over Here")
-		if(counts["Nowhere to Hide"] >= 3):
+		if(config["Nowhere to Hide"] and counts["Nowhere to Hide"] >= 3):
 			challenges.append("Nowhere to Hide")
-		if(counts["They Just... Don't... DIE!"] >= 3):
+		if(config["They Just... Don't... DIE!"] and counts["They Just... Don't... DIE!"] >= 3):
 			challenges.append("They Just... Don't... DIE!")
-		if(counts["Hold That Pose"] >= 3):
+		if(config["Hold That Pose"] and counts["Hold That Pose"] >= 3):
 			challenges.append("Hold That Pose")
-		if(counts["Where'd They Go?"] >= 3):
+		if(config["Where'd They Go?"] and counts["Where'd They Go?"] >= 3):
 			challenges.append("Where'd They Go?")
-		if(counts["Summoners on the Rift"] >= 5):
+		if(config["Summoners on the Rift"] and counts["Summoners on the Rift"] >= 5):
 			challenges.append("Summoners on the Rift")
-		if(counts["It's a Trap!"] >= 3):
+		if(config["It's a Trap!"] and counts["It's a Trap!"] >= 3):
 			challenges.append("It's a Trap!")
-		if(counts["I'm Helping"] >= 3):
+		if(config["I'm Helping"] and counts["I'm Helping"] >= 3):
 			challenges.append("I'm Helping")
-		if(counts["The Sun Disc Never Sets"] >= 5):
-			challenges.append("The Sun Disc Never Sets")
-		if(counts["Everybody was Wuju Fighting"] >= 5):
-			challenges.append("Everybody was Wuju Fighting")
-		if(counts["5 Under 5'"] >= 5):
-			challenges.append("5 Under 5'")
-		if(counts["All Hands on Deck"] >= 5):
-			challenges.append("All Hands on Deck")
-		if(counts["FOR DEMACIA"] >= 5):
-			challenges.append("FOR DEMACIA")
-		if(counts["Ice, Ice, Baby"] >= 5):
-			challenges.append("Ice, Ice, Baby")
-		if(counts["Elemental, My Dear Watson"] >= 5):
-			challenges.append("Elemental, My Dear Watson")
-		if(counts["Strength Above All"] >= 5):
-			challenges.append("Strength Above All")
-		if(counts["Calculated"] >= 5):
-			challenges.append("Calculated")
-		if(counts["Spooky Scary Skeletons"] >= 5):
-			challenges.append("Spooky Scary Skeletons")
-		if(counts["Peak Performance"] >= 5):
-			challenges.append("Peak Performance")
-		if(counts["(Inhuman Screeching Sounds)"] >= 5):
-			challenges.append("(Inhuman Screeching Sounds)")
-		if(counts["Chemtech Comrades"] >= 5):
-			challenges.append("Chemtech Comrades")
-		if(counts["assassin"] >= 5 or counts["mage"] >= 5 or counts["marksman"] >= 5 or counts["tank"] >= 5
-			or counts["support"] >= 5 or counts["figher"] >= 5):
+		if(config["Variety's Overrated"] and (
+			counts["assassin"] >= 5 
+			or counts["mage"] >= 5
+			or counts["marksman"] >= 5
+			or counts["tank"] >= 5
+			or counts["support"] >= 5
+			or counts["figher"] >= 5
+			)
+		):
 			challenges.append("Variety's Overrated")
+		if(config["The Sun Disc Never Sets"] and counts["The Sun Disc Never Sets"] >= 5):
+			challenges.append("The Sun Disc Never Sets")
+		if(config["Everybody was Wuju Fighting"] and counts["Everybody was Wuju Fighting"] >= 5):
+			challenges.append("Everybody was Wuju Fighting")
+		if(config["5 Under 5'"] and counts["5 Under 5'"] >= 5):
+			challenges.append("5 Under 5'")
+		if(config["All Hands on Deck"] and counts["All Hands on Deck"] >= 5):
+			challenges.append("All Hands on Deck")
+		if(config["FOR DEMACIA"] and counts["FOR DEMACIA"] >= 5):
+			challenges.append("FOR DEMACIA")
+		if(config["Ice, Ice, Baby"] and counts["Ice, Ice, Baby"] >= 5):
+			challenges.append("Ice, Ice, Baby")
+		if(config["Elemental, My Dear Watson"] and counts["Elemental, My Dear Watson"] >= 5):
+			challenges.append("Elemental, My Dear Watson")
+		if(config["Strength Above All"] and counts["Strength Above All"] >= 5):
+			challenges.append("Strength Above All")
+		if(config["Calculated"] and counts["Calculated"] >= 5):
+			challenges.append("Calculated")
+		if(config["Spooky Scary Skeletons"] and counts["Spooky Scary Skeletons"] >= 5):
+			challenges.append("Spooky Scary Skeletons")
+		if(config["Peak Performance"] and counts["Peak Performance"] >= 5):
+			challenges.append("Peak Performance")
+		if(config["(Inhuman Screeching Sounds)"] and counts["(Inhuman Screeching Sounds)"] >= 5):
+			challenges.append("(Inhuman Screeching Sounds)")
+		if(config["Chemtech Comrades"] and counts["Chemtech Comrades"] >= 5):
+			challenges.append("Chemtech Comrades")
 
 		# Only saving the compositions that counts for x or more challenges to save on memory and storage requirements if needed
-		# Make this CLI input
+		# TOOD : Make this CLI input
 		if(len(challenges) >= MIN_CHALLENGES):
 			compsDict[str(comp)] = challenges
 
